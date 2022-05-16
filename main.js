8c2edc976c4e029527eca177d45bffeeadcfff41
CodeWhiteWeb <79645854+CodeWhiteWeb@users.noreply.github.com>
2022-05-16 12:25:54 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,30 +1,88 @@
-function calculateSpeed(t1, lat1, lng1, t2, lat2, lng2) {
-    if (typeof(Number.prototype.toRad) === "undefined") {
-      Number.prototype.toRad = function() {
-        return this * Math.PI / 180;
-      }
+document.addEventListener("deviceready", onDeviceReady, false);
+function onDeviceReady() {
+    console.log("navigator.geolocation works well");
+}
+
+// onSuccess Callback
+// This method accepts a Position object, which contains the
+// current GPS coordinates
+//
+
+
+var onSuccess = function timer(position){
+    document.getElementById("log").innerHTML = 'Speed: '             + position.coords.speed             + '<br>' +
+          'Latitude: '          + position.coords.latitude          + '<br>' +
+          'Longitude: '         + position.coords.longitude         + '<br>' +
+          'Altitude: '          + position.coords.altitude          + '<br>' +
+          'Accuracy: '          + position.coords.accuracy          + '<br>' +
+          'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '<br>' +
+          'Heading: '           + position.coords.heading           + '<br>' +
+          'Timestamp: '         + position.timestamp                + '<br>';
+          setTimeout(timer,1000);
+        };
+// onError Callback receives a PositionError object
+//
+function onError(error) {
+    alert('code: '    + error.code    + '<br>' +
+          'message: ' + error.message + '<br>');
+}
+
+navigator.geolocation.getCurrentPosition(onSuccess, onError);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+    // onSuccess Callback
+    // This method accepts a Position object, which contains the
+    // current GPS coordinates
+    //
+    var onSuccess = function(position) {
+        alert('Latitude: '          + position.coords.latitude          + '\n' +
+              'Longitude: '         + position.coords.longitude         + '\n' +
+              'Altitude: '          + position.coords.altitude          + '\n' +
+              'Accuracy: '          + position.coords.accuracy          + '\n' +
+              'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+              'Heading: '           + position.coords.heading           + '\n' +
+              'Speed: '             + position.coords.speed             + '\n' +
+              'Timestamp: '         + position.timestamp                + '\n');
+    };
+
+    // onError Callback receives a PositionError object
+    //
+    function onError(error) {
+        alert('code: '    + error.code    + '\n' +
+              'message: ' + error.message + '\n');
     }
-    var R = 6371; // km
-    var dLat = (lat2-lat1).toRad();
-    var dLon = (lng2-lng1).toRad();
-    var lat1 = lat1.toRad();
-    var lat2 = lat2.toRad();
-  
-    var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
-      Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) *    Math.cos(lat2); 
-    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
-    var distance = R * c;
-    
-    return distance / t2 - t1;
-  }
-  
-  function firstGeolocationSuccess(position1) {
-    var t1 = Date.now();
-    navigator.geolocation.getCurrentPosition(
-      function (position2) {
-        var speed = calculateSpeed(t1 / 1000, position1.coords.latitude, position1.coords.longitude, Date.now() / 1000, position2.coords.latitude, position2.coords.longitude);
-        console.log("Speed= "+speed)  
-        document.write("Speed= "+speed)  
-    }
-    )};
-  navigator.geolocation.getCurrentPosition(firstGeolocationSuccess);
+
+    navigator.geolocation.getCurrentPosition(onSuccess, onError);
+*/
