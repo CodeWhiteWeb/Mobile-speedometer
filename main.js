c82433657da8e1aeb591bd3d492eb43fa321c7ca
CodeWhiteWeb <79645854+CodeWhiteWeb@users.noreply.github.com>
2022-05-17 13:12:37 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,32 +1,33 @@
 document.addEventListener("deviceready", onDeviceReady, false);
+
 function onDeviceReady() {
-    console.log("navigator.geolocation works well");
+  console.log("navigator.geolocation works well");
 }
 
 // onSuccess Callback
 // This method accepts a Position object, which contains the
 // current GPS coordinates
 //
-//onSuccess();
-
-var onSuccess = function(position) {
-    document.getElementById("log").innerHTML = 'Speed: '             + position.coords.speed             + '<br>' +
-          'Latitude: '          + position.coords.latitude          + '<br>' +
-          'Longitude: '         + position.coords.longitude         + '<br>' +
-          'Altitude: '          + position.coords.altitude          + '<br>' +
-          'Accuracy: '          + position.coords.accuracy          + '<br>' +
-          'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '<br>' +
-          'Heading: '           + position.coords.heading           + '<br>' +
-          'Timestamp: '         + position.timestamp                + '<br>'; 
-          setInterval(function(){ 
-            navigator.geolocation.getCurrentPosition(onSuccess);
-          }, 1000);
-        }
+var onSuccess = function (position) {
+  document.getElementById("speed").innerHTML  = Math.round(position.coords.speed * 100) / 100            + '<br>';
+  document.getElementById("lat").innerHTML    = Math.round(position.coords.latitude * 100) / 100         + '<br>';
+  document.getElementById("lng").innerHTML    = Math.round(position.coords.longitude * 100) / 100        + '<br>';
+  document.getElementById("alt").innerHTML    = Math.round(position.coords.altitude * 100) / 100         + '<br>';
+  document.getElementById("acc").innerHTML    = Math.round(position.coords.accuracy * 100) / 100         + '<br>';
+  document.getElementById("altacc").innerHTML = Math.round(position.coords.altitudeAccuracy * 100) / 100 + '<br>';
+  document.getElementById("nor").innerHTML    = Math.round(position.coords.heading * 100) / 100          + '<br>';
+  setInterval(function () {
+    navigator.geolocation.watchPosition(onSuccess);
+  }, 1000);
+}
 // onError Callback receives a PositionError object
 //
+
 function onError(error) {
-    alert('code: '    + error.code    + '<br>' +
-          'message: ' + error.message + '<br>');
+  document.getElementById("log").innerHTML = 'code: ' + error.code + '<br>' +
+    'message: ' + error.message + '<br>';
 }
 
-navigator.geolocation.getCurrentPosition(onSuccess, onError);
+navigator.geolocation.watchPosition(onSuccess, onError);
+console.log("services have been loaded");
+console.log("Created By Sajag Kamthan X C , LAHS, Gwalior");
