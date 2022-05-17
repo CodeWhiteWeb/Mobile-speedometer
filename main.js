6ba12aa7c77f7cc5c6b5d793520d8cd6591d0b5d
CodeWhiteWeb <79645854+CodeWhiteWeb@users.noreply.github.com>
2022-05-17 09:53:00 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -7,9 +7,9 @@ function onDeviceReady() {
 // This method accepts a Position object, which contains the
 // current GPS coordinates
 //
+//onSuccess();
 
-
-var onSuccess = function timer(position){
+var onSuccess = function(position) {
     document.getElementById("log").innerHTML = 'Speed: '             + position.coords.speed             + '<br>' +
           'Latitude: '          + position.coords.latitude          + '<br>' +
           'Longitude: '         + position.coords.longitude         + '<br>' +
@@ -17,9 +17,11 @@ var onSuccess = function timer(position){
           'Accuracy: '          + position.coords.accuracy          + '<br>' +
           'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '<br>' +
           'Heading: '           + position.coords.heading           + '<br>' +
-          'Timestamp: '         + position.timestamp                + '<br>';
-          setTimeout(timer,1000);
-        };
+          'Timestamp: '         + position.timestamp                + '<br>'; 
+          setInterval(function(){ 
+            navigator.geolocation.getCurrentPosition(onSuccess);
+          }, 1000);
+        }
 // onError Callback receives a PositionError object
 //
 function onError(error) {
@@ -28,61 +30,3 @@ function onError(error) {
 }
 
 navigator.geolocation.getCurrentPosition(onSuccess, onError);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-    // onSuccess Callback
-    // This method accepts a Position object, which contains the
-    // current GPS coordinates
-    //
-    var onSuccess = function(position) {
-        alert('Latitude: '          + position.coords.latitude          + '\n' +
-              'Longitude: '         + position.coords.longitude         + '\n' +
-              'Altitude: '          + position.coords.altitude          + '\n' +
-              'Accuracy: '          + position.coords.accuracy          + '\n' +
-              'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
-              'Heading: '           + position.coords.heading           + '\n' +
-              'Speed: '             + position.coords.speed             + '\n' +
-              'Timestamp: '         + position.timestamp                + '\n');
-    };
-
-    // onError Callback receives a PositionError object
-    //
-    function onError(error) {
-        alert('code: '    + error.code    + '\n' +
-              'message: ' + error.message + '\n');
-    }
-
-    navigator.geolocation.getCurrentPosition(onSuccess, onError);
-*/
